0eb2020cfa3b9b52a43818888820509d24edab42
unknown <dmphuc21@clc.fitus.edu.vn>
2024-01-18 16:28:02 +0700

    finish layout for liked button

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-
+import Image from "next/image"
+import {FaPlay} from "react-icons/fa"
 interface ListItemProps{
     image: string;
     name: string;
@@ -38,7 +39,35 @@ const ListItem: React.FC<ListItemProps> = ({
                 min-h-[64px]
                 min-w-[64px]
             ">
-
+                <Image
+                 className="object-cover"
+                 fill
+                 src={image}
+                 alt="image"
+                />
+                
+            </div>
+            <p className="font-medium truncate py-5">
+                {name}
+            </p>
+            <div
+             className="
+                absolute
+                transition
+                opacity-0
+                rounded-full
+                flex
+                items-center
+                justify-center
+                bg-green-500
+                p-4
+                drop-shadow-md
+                right-5
+                group-hover:opacity-100
+                hover:scale-110
+             "
+            >
+                <FaPlay className="text-black"/>
             </div>
         </button>
     )
