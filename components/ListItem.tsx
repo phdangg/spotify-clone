ae1062eac8d4fb2fbda43d6a8ced103b4fb714ce
unknown <dmphuc21@clc.fitus.edu.vn>
2024-01-18 16:12:59 +0700

    create button box for ListItem

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,9 +1,46 @@
-  
-const ListItem = () =>{
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface ListItemProps{
+    image: string;
+    name: string;
+    href: string;
+}
+const ListItem: React.FC<ListItemProps> = ({
+    image,
+    name,
+    href
+}) =>{
+    const router = useRouter();
+    const onClick = () =>{
+        //Add authentication before push
+        router.push(href);
+    }
     return (
-        <div>
-            ListItem
-        </div>
+        <button
+         className="
+            relative
+            group
+            flex
+            items-center
+            rounded-md
+            overflow-hidden
+            gap-x-4
+            bg-neutral-100/10
+            hover:bg-neutral-100/20
+            transition
+            pr-4
+         "
+        >
+            <div className="
+                relative
+                min-h-[64px]
+                min-w-[64px]
+            ">
+
+            </div>
+        </button>
     )
 }
 
