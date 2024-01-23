accdc0ff7f3f305d700bc2c659ffa2d175dfe8eb
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-23 22:54:02 +0700

    create modal provider

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
+const ModalProvider = () =>{
+    const [isMounted,setIsMounted] = useState(false);
+    
+    return (
+        <>
+            Modals
+        </>
+    )
+}
