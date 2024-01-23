98b400a3a9f412d51ac5884540ad44f32b6e73d9
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-23 23:09:00 +0700

    create ModalProps interface

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,3 +1,14 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import {IoMdClose} from "react-icons/io";
+
+interface ModalProps{
+    isOpen: boolean;
+    onChange: (open: boolean) => void;
+    title: string;
+    description: string;
+    children: React.ReactNode
+}
+
 const Modal = () => {
     return (
         <div>
