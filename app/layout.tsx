cb8126bd24ccb8210f9db6edada5381305661aaa
unknown <dmphuc21@clc.fitus.edu.vn>
2024-01-23 21:26:37 +0700

    wrap SideBar with SbProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import './globals.css'
 import Sidebar from '@/components/Sidebar'
+import SuperbaseProvider from '@/providers/SuperbaseProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>
-          {children}
-        </Sidebar>
+        <SuperbaseProvider>
+          <Sidebar>
+            {children}
+          </Sidebar>`
+        </SuperbaseProvider>
       </body>
     </html>
   )
