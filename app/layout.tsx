1551be3ff09a0eb615055559c77e4a94b73ab446
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-24 20:49:52 +0700

    add ToasterProvider component

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Sidebar from '@/components/Sidebar'
 import SuperbaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider/>
         <SuperbaseProvider>
           <UserProvider>
             <ModalProvider />
