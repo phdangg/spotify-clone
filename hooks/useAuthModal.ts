64759cd8f2c4f7b0d11091e7219e5328900a0c26
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-24 19:08:11 +0700

    create custom useAuthModal hook

--- hooks/useAuthModal.ts
+++ hooks/useAuthModal.ts
@@ -5,3 +5,11 @@ interface AuthModalStore{
     onOpen: ()=>void;
     onClose: ()=>void;
 }
+
+const useAuthModal = create<AuthModalStore>((set)=>({
+    isOpen: false,
+    onOpen: ()=>set({isOpen:true}),
+    onClose: ()=>set({isOpen:false})
+}))
+
+export default useAuthModal;
