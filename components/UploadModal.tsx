e38f62e2a0cfa73dbd579294293fd4f2ebe57492
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-25 15:34:03 +0700

    open/close the upload modal

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,12 +1,20 @@
+"use client";
+import useUploadModal from "@/hooks/useUploadModal";
 import Modal from "./Modal";
 
 const UploadModal = () => {
+    const uploadModal = useUploadModal();
+    const onChange = (open: boolean) =>{
+        if (!open){
+            uploadModal.onClose();
+        }
+    }
     return (
         <Modal
         title="Upload modal title"
         description="Upload modal description"
-        isOpen
-        onChange={()=>{}}>
+        isOpen={uploadModal.isOpen}
+        onChange={onChange}>
             Upload Content
         </Modal>
     );
