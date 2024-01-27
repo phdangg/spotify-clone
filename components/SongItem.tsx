d3a5ef7344c7a8ca7d6a5d310546fcd5fb094211
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-27 17:34:18 +0700

    create interface for SongItem

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import {Song} from "@/types";
+
+interface SongItemProps{
+    data: Song;
+    onClick: (id: string) => void;
+};
+
 const SongItem = () =>{
     return (
         <div></div>
