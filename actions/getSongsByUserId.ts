b265dd0c3174561c7064a858638395eef7c5cff9
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-28 16:27:30 +0700

    check if session is valied

--- actions/getSongsByUserId.ts
+++ actions/getSongsByUserId.ts
@@ -6,7 +6,14 @@ const getSongsByUserID = async (): Promise<Song[]> => {
     const supabase = createServerComponentClient({
         cookies: cookies
     });
-
+    const {
+        data: sessionData, 
+        error:sessionError
+    } = await supabase.auth.getSession();
+    if (sessionError){
+        console.log(sessionError.message);
+        return []
+    }
     
 }
 
