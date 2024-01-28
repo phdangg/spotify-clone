3eaf37bcaaf911ca8b62635c27315f3c8fe91f69
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-28 16:49:07 +0700

    styling for each song box

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -2,6 +2,7 @@
 
 import useLoadingImage from "@/hooks/useLoadingImage";
 import {Song} from "@/types";
+import Image from "next/image";
 
 interface MediaItemProps {
     data: Song;
@@ -14,10 +15,52 @@ const MediaItem: React.FC<MediaItemProps> = ({
 }) => {
     const imageUrl = useLoadingImage(data);
     const handleClick=  () => {
-        
+        if (onClick){
+            return onClick(data.id);
+        }
     }
     return (
-        <div>Media</div>
+        <div
+        onClick={handleClick}
+        className="
+            flex
+            items-center
+            gap-x-3
+            cursor-pointer
+            hover:bg-neutral-800/50
+            w-full
+            p-2
+            rounded-md
+        ">
+            <div
+            className="
+                relative
+                rounded-md
+                min-h-[48px]
+                min-w-[48px]
+                overflow-hidden
+            ">
+                <Image
+                fill
+                src={imageUrl || '/images/liked.png'}
+                alt="Media Item"
+                className="object-cover"
+                />
+            </div>
+            <div className="
+                flex
+                flex-col
+                gap-y-1
+                overflow-hidden
+            ">
+                <p className="text-white truncate">
+                  {data.title}  
+                </p>
+                <p className="text-neutral-400 truncate">
+                  {data.author}  
+                </p>
+            </div>
+        </div>
     );
 }
 export default MediaItem;
