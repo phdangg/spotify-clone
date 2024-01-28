db4d23a2477e1ecd406cfa71d1ad2fd8ec0c5215
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-28 16:11:04 +0700

    display titel,author

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -52,7 +52,28 @@ const SongItem: React.FC<SongItemProps> = ({
                     alt="Image"
                 />
             </div>
-            Song Item
+            <div className="flex flex-col items-start w-full pt-4 gap-y-1">
+                <p className="font-semibold truncate w-full">
+                    {data.title}
+                </p>
+                <p
+                className="
+                    text-neutral-400
+                    text-sm
+                    pb-4
+                    w-full
+                    truncate
+                ">
+                    By {data.author}
+                </p>
+            </div>
+            <div className="
+                absolute
+                bottom-24
+                right-5
+            ">
+                Play Button
+            </div>
         </div>
     )   
 }
