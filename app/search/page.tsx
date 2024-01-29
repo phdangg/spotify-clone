2d15e1283390006bf5831cf8023dd30e0e1cbf9f
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-29 10:02:48 +0700

    create Search FC

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,17 @@
+import getSongsByTitle from "@/actions/getSongsByTitle";
+
 interface SearchProps {
     searchParams: {
         title: string;
     }
 }
+
+const Search = async ({searchParams}: SearchProps) => {
+    const songs = getSongsByTitle(searchParams.title);
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default Search;
