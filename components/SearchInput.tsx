94f5bdfc750bdf4fe8692708b4b9c2330aee3291
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-29 10:17:02 +0700

    using useRouter and useDebounce

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,6 +1,13 @@
 "use client";
 
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 const SearchInput = () => {
+    const router = useRouter();
+    const [value,setValue] = useState<string>("");
+    const debounceValue = useDebounce<string>(value,500);
     return (
         <div></div>
     )
