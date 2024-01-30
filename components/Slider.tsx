1677b3812ab302cb262bed8609ef730293501712
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-30 15:23:25 +0700

    interface for Slider

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -2,7 +2,16 @@
 
 import * as RadixSlider from "@radix-ui/react-slider"   
 
-const Slider = () => {
+interface SliderProps {
+    value?: number;
+    onChange?: (value: number) => void;
+}
+
+const Slider: React.FC<SliderProps> = ({
+    value = 1,
+    onChange
+}) => {
+   
     return (
       <div>Slider</div>  
     );
