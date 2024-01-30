c8e352459b5efc6e891ecf7518dc35d83c4d65af
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-30 11:13:28 +0700

    create LikeContent FC

---
+++ components/LikedContent.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Song } from "@/types";
+
+interface LikedContent {
+    songs: Song[];
+}
+
+const LikedContent: React.FC<LikedContent> = ({
+    songs
+}) => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+export default LikedContent;
