7e10cbb7d3e380ed60d266895b3be7b4b2cf73bf
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-30 16:13:05 +0700

    style layout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,6 +8,8 @@ import { Song } from "@/types";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
 import Library from "./Library";
+import usePlayer from "@/hooks/usePlayer";
+import { twMerge } from "tailwind-merge";
 interface SidebarProps{
     children: React.ReactNode;
     songs: Song[];
@@ -17,6 +19,8 @@ const Sidebar: React.FC<SidebarProps> = ({
     songs
 }) => {
     const pathname = usePathname();
+    const player = usePlayer();
+
     const routes = useMemo(()=>[
         {
             icon: HiHome,
@@ -32,7 +36,10 @@ const Sidebar: React.FC<SidebarProps> = ({
             },
     ],[pathname])
     return (
-        <div className="flex h-full">
+        <div className={twMerge(`
+            flex
+            h-full
+        `,player.activeId && "h-[calc(100%-80px)]")}>
             <div 
                 className="
                     hidden
