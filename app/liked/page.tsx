7afd9de53ebd673a6e1cd7d15f9f4c473f2a46d1
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-30 11:20:29 +0700

    import LikedSong compo

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -1,5 +1,6 @@
 import getLikedSongs from "@/actions/getLikedSongs";
 import Header from "@/components/Header";
+import LikedContent from "@/components/LikedContent";
 import Image from "next/image";
 
 export const revalidate = 0;
