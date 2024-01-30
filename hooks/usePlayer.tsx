41e05a3cc976f26bfb72162ad8703e0e1d03ef13
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-30 11:27:22 +0700

    create a store

--- hooks/usePlayer.tsx
+++ hooks/usePlayer.tsx
@@ -1,3 +1,5 @@
+import { create } from "zustand";
+
 interface PlayerStore {
     ids: string[];
     activeId?: string;
@@ -5,3 +7,11 @@ interface PlayerStore {
     setIds: (ids: string[]) => void;
     reset: () => void;
 }
+
+const usePlayer = create<PlayerStore>((set)=>({
+    ids: [],
+    activeId: undefined,
+    setId: (id: string) => set({activeId:id}),
+    setIds: (ids: string[]) => set({ids:ids}),
+    reset: () => set({ids: [],activeId: undefined})
+}));
