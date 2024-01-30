53054d69be55307bedbe4315598258f003229a09
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-01-30 15:07:37 +0700

    forward,backward and play icon

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { Song } from "@/types";
-import {BsPauseFill,BsPlayFill} from "react-icons/bs"
+import { BsPauseFill,BsPlayFill } from "react-icons/bs"
+import { HiSpeakerXMark, HiSpeakerWave } from "react-icons/hi2"
 import MediaItem from "./MediaItem";
 import LikeButton from "./LikeButton";
+import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 
 interface PlayerContent {
     song: Song;
@@ -15,6 +17,7 @@ const PlayerContent: React.FC<PlayerContent> = ({
     songUrl
 }) => {
     const Icon = true ? BsPauseFill : BsPlayFill;
+    const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
     return (
         <div className="grid grid-cols-2 md:grid-cols-3 h-full">
             <div className="
@@ -49,8 +52,57 @@ const PlayerContent: React.FC<PlayerContent> = ({
                     cursor-pointer
                 ">
                     <Icon size={30} className="text-black"/>
+                </div>                
+            </div>
+            <div className="
+                hidden
+                h-full
+                md:flex
+                justify-center
+                items-center
+                w-full
+                max-w-[722px]
+                gap-x-6
+            ">
+                <AiFillStepBackward 
+                size={30} 
+                className="
+                    text-neutral-400
+                    cursor-pointer
+                    hover:text-white
+                    transition
+                "/>
+                <div 
+                onClick={()=>{}}
+                className="
+                    flex
+                    items-center
+                    justify-center
+                    h-10
+                    w-10
+                    rounded-full
+                    bg-white
+                    p-1
+                    cursor-pointer
+                ">
+                    <Icon size={30} className="text-black"/>
                 </div>
-                
+                <AiFillStepForward
+                onClick={()=>{}}
+                size={30} 
+                className="
+                    text-neutral-400
+                    cursor-pointer
+                    hover:text-white
+                    transition
+                "
+                />
+            </div>
+
+            <div className="hidden md:flex w-full justify-end pr-2">
+                <div className="flex items-center gap-x-2 w-[120px]">
+                    <VolumeIcon/>
+                </div>
             </div>
         </div>
     )
